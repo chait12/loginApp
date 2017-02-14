56d9098f9709bb90c931ba6be1d7d88c74fa150f
ravi_shah <ravi.shah@raweng.com>
2017-02-15 02:36:24 +0530

    develop

--- controllers/users.js
+++ controllers/users.js
@@ -8,7 +8,7 @@ router.post('/signup', createUser);
 
 function createUser(req, res, next){
   User.create(req.body, function(err, doc){
-    console.log(err, doc)
+    console.log(err, doc);
     if(!err && doc){
       return res.send(201).json(doc);
     }
